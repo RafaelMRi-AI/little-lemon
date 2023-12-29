9d8bfa980f7ff2115098e6592fa7b540542b7a7e
Rafael MRi <rfm.lnx.ai@gmail.com>
2023-12-29 02:01:07 -0600

    Little Lemon Commit 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 
 import './App.css';
 
+import React from 'react';
+import Header from './Components/Header';
+import Nav from './Components/Nav';
+import Main from './Components/Main';
+import Footer from './Components/Footer';
+
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
 export default App;
+
+
---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+  <footer>
+    {/* Your footer content goes here */}
+    &copy; 2023 Little Lemon. All rights reserved.
+    </footer>
+    );
+}
+
+export default Footer;
